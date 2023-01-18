b44e49324cafd175065e5d8dac6bcaa5cd1f95b1
Karolina <ka.kolomyjec@gmail.com>
2023-01-18 20:09:00 +0100

    finished

--- checking numbers/script.js
+++ checking numbers/script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+function writeNumbers () {
+
+    let number1 = document.getElementById("field1").value;
+    let number2= document.getElementById("field2").value;
+    let text = "";
+
+    for(let i = number1; i<= number2; i++ ){
+        
+        text = text + i + "";
+
+    }
+
+    document.getElementById("results").innerHTML = text;
+
+
+}
+
+const button = document.getElementById("button");
+button.addEventListener("click", writeNumbers);
+
