75105e24d751af9dd8a6c99e87220f39546742cf
Karolina <ka.kolomyjec@gmail.com>
2023-01-22 16:32:01 +0100

    add function that writes slogans and startin to do the function that show letters to guess

--- Hangman/script.js
+++ Hangman/script.js
@@ -1,9 +1,29 @@
 'use strict'
 
 let password = "Bez pracy nie ma kolaczy";
+password = password.toUpperCase();
 
+let sizePassword = password.length;
+
+let password1 = "";
+// loop that hide password for us
+for(let i=0; i<sizePassword;i++){
+    if (password.charAt(i)==" ") password1 = password1 + " ";
+    else password1 = password1 + "-";
+}
+//function that writes password on the top 
 function write_password (){
-    document.getElementById("board").innerHTML = password;
+    document.getElementById("board").innerHTML = password1;
 }
 
-window.onload = write_password; 
+window.onload = start; 
+
+//write letters in alphabet
+
+function start(){
+
+    document.getElementById("alphabet").innerHTML = "";
+
+    write_password();
+
+}
