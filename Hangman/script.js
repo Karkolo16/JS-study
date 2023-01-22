b3baa96e77a8c7134e99a7a5a1767626f0377ddd
Karolina <ka.kolomyjec@gmail.com>
2023-01-22 16:01:51 +0100

    add functiopn write_password to JS

--- Hangman/script.js
+++ Hangman/script.js
@@ -1,2 +1,9 @@
 'use strict'
 
+let password = "Bez pracy nie ma kolaczy";
+
+function write_password (){
+    document.getElementById("board").innerHTML = password;
+}
+
+window.onload = write_password; 
