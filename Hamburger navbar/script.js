a27a27209d20f750e2bff55ec2afe4b7a3c67be9
Karolina <ka.kolomyjec@gmail.com>
2023-02-04 17:49:54 +0100

    add JS to hamburger

--- Hamburger navbar/script.js
+++ Hamburger navbar/script.js
@@ -1,2 +1,8 @@
 'use strict'
 
+const toggler = document.getElementsByClassName('toggler')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggler.addEventListener('click', () =>{
+    navbarLinks.classList.toggle('active')
+})
