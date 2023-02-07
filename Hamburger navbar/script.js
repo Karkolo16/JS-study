73bd8dcec16977f634cf300b1c2619ce9d7e2c8a
Karolina <ka.kolomyjec@gmail.com>
2023-02-07 20:15:31 +0100

    add FILES

--- Hamburger navbar/script.js
+++ Hamburger navbar/script.js
@@ -1,8 +1,2 @@
 'use strict'
 
-const toggler = document.getElementsByClassName('toggler')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-
-toggler.addEventListener('click', () =>{
-    navbarLinks.classList.toggle('active')
-})
