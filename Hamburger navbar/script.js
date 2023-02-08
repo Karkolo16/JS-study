9063072656a3a214f41f933c14e491d8f7438937
Karolina <ka.kolomyjec@gmail.com>
2023-02-08 19:04:51 +0100

    fixed

--- Hamburger navbar/script.js
+++ Hamburger navbar/script.js
@@ -1,2 +1,9 @@
 'use strict'
 
+const toggleButton = document.getElementsByClassName('toggler')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () =>{
+    navbarLinks.classList.toggle('active')
+})
+
