3ed43517e8709d5be65df83c6344ab9526eb93c8
Karolina <ka.kolomyjec@gmail.com>
2023-02-27 18:33:50 +0100

    make arrat and const button in JS

---
+++ learning/script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const quotes = [
+        { 
+        "author": "Koło",
+        "quote": "asfjskfdjasldasklda"
+       },
+
+       { 
+        "author": "Koło2",
+        "quote": "asfjskfdjasldasklda"
+       },
+
+   { 
+    "author": "Koło3",
+    "quote": "asfjskfdjasldasklda"
+   }
+]
+
+
+
+const button = document.getElementById("btn");
