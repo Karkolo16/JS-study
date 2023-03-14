8be140cf99cb013d18dd6a91888c942fbbcf9e7d
Karolina <ka.kolomyjec@gmail.com>
2023-03-14 18:14:06 +0100

    add popup_img to show clicked photo

--- photo gallery htmlcssjs/app.js
+++ photo gallery htmlcssjs/app.js
@@ -3,14 +3,17 @@
 const thumbnails = document.querySelectorAll(".thumbnail img");
 const popup = document.querySelector(".popup");
 const popup_close = document.querySelector(".popup__close");
+const popup_img= document.querySelector(".popup__img");
 
 thumbnails.forEach((thumbnail) => {
 
-    thumbnail.addEventListener('click', () =>{
-        popup.classList.remove("hidden")
+    thumbnail.addEventListener('click', (e) =>{
+        popup.classList.remove("hidden");
+        popup_img.src = e.target.src;
     })
 });
 
 popup_close.addEventListener('click',  () =>{
     popup.classList.add("hidden");
-})
+});
+
