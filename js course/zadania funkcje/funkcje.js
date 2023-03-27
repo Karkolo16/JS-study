2acd6e032521111434e6b88202cdb63603f1bfaa
Karolina <ka.kolomyjec@gmail.com>
2023-03-27 19:18:15 +0200

    fonished first excercise

--- js course/zadania funkcje/funkcje.js
+++ js course/zadania funkcje/funkcje.js
@@ -1,5 +1,17 @@
 //Korzystając z wiedzy z segmentu Funkcje, napisz następujące funkcje:
 
-//Napisz funkcję, która przemnoży liczbę przekazaną w parametrze przez dwa i wypisze wynik na konsolę.
-//Napisz funkcję, która przemnoży liczbę przekazaną w parametrze przez dowolną liczbę, przekazaną w kolejnym parametrze.
-//Napisz funkcję, która podniesie liczbę przekazaną w parametrze do potęgi drugiej.
+//Napisz funkcję, która przemnoży liczbę przekazaną w parametrze przez dwa i wypisze wynik na konsolę. A
+//Napisz funkcję, która przemnoży liczbę przekazaną w parametrze przez dowolną liczbę, przekazaną w kolejnym parametrze. b
+//Napisz funkcję, która podniesie liczbę przekazaną w parametrze do potęgi drugiej. C
+
+let timesTwo = (number) => {
+  console.log(number * 2);
+};
+
+let timesInfinity = (a, b) => {
+  console.log(a * b);
+};
+
+let squareValue = (num) => {
+  console.log(num * num);
+};
