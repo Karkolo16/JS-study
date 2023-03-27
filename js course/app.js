6b4c435c71f14a1bb3976fae75caa53af2010f1b
Karolina <ka.kolomyjec@gmail.com>
2023-03-27 19:50:37 +0200

    add id to dot

--- js course/app.js
+++ js course/app.js
@@ -1,14 +1,4 @@
-let name = "Karolina";
-let lastName = "Programistka";
-console.log(name + " " + lastName);
-
-name = "Jan";
-lastName = "Kowalski";
-console.log(name + " " + lastName);
-
-let person1 = {
-  name: "Karolina",
-  lastName: "Programistka",
-};
-
-console.log(person1.name + " " + person1.lastName);
+let arrowLeft = document.querySelector(".arrow-left");
+console.log(arrowLeft);
+let arrowRight = document.querySelector(".arrow-right");
+console.log(arrowRight);
