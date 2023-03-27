36e6bcd9e7950dec8a776f178b6601764dd67267
Karolina <ka.kolomyjec@gmail.com>
2023-03-27 19:42:24 +0200

    finnished objects excercise

---
+++ js course/zadania obiekty/obiekt.js
@@ -0,0 +1,23 @@
+//Korzystając z wiedzy zdobytej w sekcji Obiekty stwórz dwa obiekty reprezentujące dwa samochody. Obiekty powinny składać się z trzech właściwości:
+
+//marki pojazdu (nazwa właściwości mark),
+//modelu pojazdu (nazwa właściwości model),
+//roku produkcji (nazwa właściwości productionYear)
+//Obiekty te przypisz do zmiennych car1 i car2.
+
+//Na sam koniec , dla każdego z obiektów na konsole wypisz markę, model i rok produkcji pojazdu.
+// Przykładowy format wiadomości: Fiat Punto, rok produkcji: 2021.
+
+car1 = {
+  mark: "Ford",
+  model: "KA",
+  productionYear: "2010",
+};
+
+car2 = {
+  mark: "Skoda",
+  model: "CityGo",
+  productionYear: "2014",
+};
+
+console.log(car2.mark + " " + car2.model + " " + car2.productionYear);
