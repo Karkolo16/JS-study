d2d14cc76e3654d71dd6c258712e2d4f1a83311c
Karolina <ka.kolomyjec@gmail.com>
2023-03-29 19:07:04 +0200

    add slides to js

--- js course/app.js
+++ js course/app.js
@@ -3,6 +3,10 @@ console.log(arrowLeft);
 let arrowRight = document.querySelector(".arrow-right");
 console.log(arrowRight);
 
-let dot1 = document.querySelector("#dot");
+let dot1 = document.querySelector("#dot1");
 let dot2 = document.querySelector("#dot2");
 let dot3 = document.querySelector("#dot3");
+
+let slide1 = document.querySelector("#slide1");
+let slide2 = document.querySelector("#slide2");
+let slide3 = document.querySelector("#slide3");
