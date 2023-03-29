29d9607b380aa2fc42eee01042017e456b2f4052
Karolina <ka.kolomyjec@gmail.com>
2023-03-29 19:17:10 +0200

    FINISHED EXCERSISE WITH VARIABLES

---
+++ js course/zadania zmienne/zmienne.js
@@ -0,0 +1,13 @@
+//Zmienną C, która będzie reprezentować prędkość światła. Przyjmij, ze prędkość światła to 299792458,
+//Wypisz wartość zmiennej C na konsolę,
+//Najsłynniejszy wzór fizyczny to E=MC^2. Pozwala on obliczyć energię ciała o masie M. Stwórz zmienną M,
+//która będzie przechowywać masę obiektu, która wynosi 10. Uzywając zmiennych, oblicz całkowitą energię ciała.
+//Przypisz ją do zmiennej E i wypisz ją na konsolę.
+
+let C = 299792458;
+console.log(C);
+
+let M = 10;
+let E = M * C * C;
+
+console.log(E);
