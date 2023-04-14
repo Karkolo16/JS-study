6701f3c25f1a1ef391c128cd3092f7b382c11678
Karolina <ka.kolomyjec@gmail.com>
2023-04-14 09:59:43 +0200

    add array

--- SelfProject/app.js
+++ SelfProject/app.js
@@ -3,3 +3,18 @@ let button = document.getElementById("change");
 button.addEventListener("click", () => {
   document.body.classList.toggle("color");
 });
+
+let quotes = [
+  {
+    author: "aaaaaa",
+    quote: "la la lavida loca",
+  },
+  {
+    author: "bbbbbbb",
+    quote: "pencil",
+  },
+  {
+    author: "aaaaaa",
+    quote: "la la lavida loca",
+  },
+];
