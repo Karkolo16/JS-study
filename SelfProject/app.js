609267e76767f59d987d5cd433c8f0911dd422f8
Karolina <ka.kolomyjec@gmail.com>
2023-04-16 16:57:31 +0200

    finished with random quotes

--- SelfProject/app.js
+++ SelfProject/app.js
@@ -6,23 +6,36 @@ button.addEventListener("click", () => {
 
 let quotes = [
   {
-    author: "aaaaaa",
-    quote: "la la lavida loca",
+    "author": "aaaaaa",
+    "quote": "la la lavida loca",
   },
   {
-    author: "bbbbbbb",
-    quote: "pencil",
+    "author": "bbbbbbb",
+    "quote": "pencil",
   },
   {
-    author: "aaaaaa",
-    quote: "la la lavida loca",
+    "author": "aaaaaa",
+    "quote": "la la lavida loca",
   },
 ];
 
 //znajdz przycisk toggle
-let btn = document.getElementById("#toggle");
+let btn = document.getElementById("toggle");
 
 //funkcja losujaca cytaty
 function randomSelector(arrayLength) {
-  Math.floor(Math.random * arrayLength);
+  return Math.floor(Math.random() * arrayLength);
 }
+//funkcja generujaca widoczne cytatyi ich autorow
+function quoteGenerator() {
+  let randomNumber = randomSelector (quotes.length);
+
+   document.getElementById("quoteOutput").innerHTML =
+    '"' + quotes[randomNumber].quote + '"';
+  document.getElementById("authorOutput").innerHTML =
+    "-" + quotes[randomNumber].author + "-";
+}
+
+btn.addEventListener('click', function () {
+  quoteGenerator();
+});
