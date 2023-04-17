af85fc77c674978c0313df6b5855f8516d0f87fd
Karolina <ka.kolomyjec@gmail.com>
2023-04-17 18:12:55 +0200

    add files

--- checking numbers/script.js
+++ checking numbers/script.js
@@ -1,24 +1,20 @@
-'use strict'
+"use strict";
 
-function writeNumbers () {
+function writeNumbers() {
+  let number1 = document.getElementById("field1").value;
+  let number2 = document.getElementById("field2").value;
+  let text = "";
 
-    let number1 = document.getElementById("field1").value;
-    let number2= document.getElementById("field2").value;
-    let text = "";
+  for (let i = number1; i <= number2; i++) {
+    text = text + i + " ";
+  }
 
-    for(let i = number1; i<= number2; i++ ){   
-        text = text + i + " ";
-    }
-    
-    for(let i = number1; i >= number2; i--){
-        text = text + i + " ";
-    }
+  for (let i = number1; i >= number2; i--) {
+    text = text + i + " ";
+  }
 
-    document.getElementById("results").innerHTML = text;
-
-
+  document.getElementById("results").innerHTML = text;
 }
 
 const button = document.getElementById("button");
 button.addEventListener("click", writeNumbers);
-
